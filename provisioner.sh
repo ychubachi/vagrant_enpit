04d3a73f8f2e4737b4657ecb3c2c50dd2ffbe96c
Yoshihide Chubachi <yoshi@chubachi.net>
2019-06-12 01:32:00 +0900

    ruby 2.5.5のインストールまで完了

---
+++ provisioner.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# update apt repository
+apt-get update
+
+# tools
+apt-get install -y emacs
+
+# postgres
+apt-get install -y postgresql
+systemctl enable postgresql
+
+# create a database for vagrant
+/etc/init.d/postgresql start
+sudo -u postgres createuser -d vagrant
+sudo -u postgres createdb vagrant
+/etc/init.d/postgresql stop
+
+# install heroku tool belt
+snap install --classic heroku
+
+# ruby
+apt-get install -y autoconf bison build-essential \
+  libssl-dev libyaml-dev libreadline6-dev zlib1g-dev \
+  libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+sudo su - vagrant <<'EOF'
+  cd ~
+
+  # rbenv
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  echo 'export PATH=~/.rbenv/bin:$PATH' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+  # ruby-build
+  mkdir -p ~/.rbenv/plugins
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+  # install ruby
+  ~/.rbenv/bin/rbenv install 2.5.5
+  ~/.rbenv/bin/rbenv global 2.5.5
+EOF
